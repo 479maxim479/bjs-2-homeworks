f4df76cc61fea686b83f07b7d03a7491f86c6706
479maxim479 <479maxim479@gmail.com>
2022-09-09 15:08:47 +0300

    modified task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,22 +2,18 @@
 
 function solveEquation(a, b, c) {
   const arr = [];
-  const Discriminant = b ** 2 - 4 * a * c;
+  const discriminant = b ** 2 - 4 * a * c;
   let x1;
   let x2;
 
-  if (Discriminant < 0) {
-    return arr;
-  }
-
-  if (Discriminant == 0) {
+  if (discriminant == 0) {
     x1 = -b / (2 * a)
     arr[0] = x1;
   }
 
-  if (Discriminant > 0) {
-    x1 = (-b + Math.sqrt(Discriminant)) / (2 * a);
-    x2 = (-b - Math.sqrt(Discriminant)) / (2 * a);
+  if (discriminant > 0) {
+    x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
 
     arr[0] = x1;
     arr[1] = x2;
