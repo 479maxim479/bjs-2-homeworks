bd9fc8c878701ba927c43d09828ec755db5349c5
479maxim479 <479maxim479@gmail.com>
2022-09-12 17:40:42 +0300

    first commit arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,19 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  let result = true;
 
-  // Ваш код
+  arr1.every((element1) => [element1]);
+  arr2.every((element2) => [element2]);
 
-  return result; // boolean
+  if (arr1.length === arr2.length && arr1[0] === arr2[0]) {
+    return result;
+  }
+  return !result;
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  let positiveElement = arr.filter((element) => element > 0);
+  let multiple = positiveElement.filter((element) => element % 3 === 0)
+  let resultArr = multiple.map((element) => element * 10);
 
-  // Ваш код
-
-  return resultArr; // array
+  return resultArr;
 }
