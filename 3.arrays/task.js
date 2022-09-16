c00c22c9f4823801b97840f68f897287a7643181
479maxim479 <479maxim479@gmail.com>
2022-09-16 15:20:09 +0300

    second commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,18 +1,9 @@
 function compareArrays(arr1, arr2) {
-  let result = true;
-
-  arr1.every((element1) => [element1]);
-  arr2.every((element2) => [element2]);
-
-  if (arr1.length === arr2.length && arr1[0] === arr2[0]) {
-    return result;
-  }
-  return !result;
+  let result = arr1.length === arr2.length && arr1.every((el, i) => el === arr2[i]);
+  return result;
 }
 
 function advancedFilter(arr) {
-  let checkArr = arr.filter((element) => element > 0).filter(element => element % 3 === 0);
-  let resultArr = checkArr.map((element) => element * 10);
-
+  let resultArr = arr.filter(element => element > 0).filter(element => element % 3 === 0).map(element => element * 10);
   return resultArr;
 }
